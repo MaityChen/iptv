71422d0c031c465d348e3329f887ad10059e0422
MaityChen <maity@vip.qq.com>
2022-06-07 16:07:48 +0800

    Update s001.js

--- s001.js
+++ s001.js
@@ -1,17 +1,12 @@
+var inputKeyword = document.getElementById('maitysearch');
+var btnSearch = document.getElementById('maitysearchbtn');
+var text = document.getElementById('maitycontent');
 
-function maitySearch() {
-    var content = $('#content').html();
-    $('#search-btn').click(function () {
-        $('#content').html(content);
-        var searchText = $('#search').val();
-        if (searchText.length == 0) {
-            return false;
-        }
-        var regExp = new RegExp(searchText, 'g');
-        var newHtml = content.replace(regExp, '<span id="result" style="background:yellow;color:red;">' + searchText + '</span>');
-        $('#content').html(newHtml);
-        var X = $('#result').offset().top;
-        var Y = $('#result').offset().left;
-        window.scrollTo(X, Y);
-    });
+btnSearch.onclick = function () {
+    var keyword = inputKeyword.value;
+    if (!keyword) {
+        return;
+    }
+    var textSplit = text.innerHTML.split(keyword);
+    text.innerHTML = textSplit.join('<span class="keywordBg">' + keyword + '</span>');
 }
